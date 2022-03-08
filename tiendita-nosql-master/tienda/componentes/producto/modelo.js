dcca307040ee51e7c9dff56c635d3cda90e0d32f
Abraham Paladines <abrahampaladines@hotmail.es>
2022-03-07 19:54:27 -0500

    examen

---
+++ tiendita-nosql-master/tienda/componentes/producto/modelo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const reqString = {
+    type: String,
+    required: true,
+}
+
+const reqNumber = {
+    type: Number,
+    required: true,
+}
+
+const proveedorSchema = new Schema({
+    nombre: reqString,
+    domicilio: reqString,
+})
+
+const productoSchema = new Schema({
+    serie: reqString,
+    codigo: reqString,
+    nombre: reqString,
+    valor: reqNumber,
+    ref_proveedor: [proveedorSchema],
+})
+
+const model = mongoose.model('Producto', productoSchema)
+module.exports = model
